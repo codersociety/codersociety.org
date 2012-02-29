a3d8108dfb80224aafbf82930b5d526bdb80e21d
Adron Hall - Macbook Air <adronhall@gmail.com>
2012-02-29 14:26:11 -0800

    Just the latest bits, working on routing and the jquery tabs.

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' });
+};
+
+exports.about = function(req, res){
+    res.render('about', { title: 'About' })
 };
